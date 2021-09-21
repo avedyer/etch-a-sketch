08cc06b62a013d05df9bd26f0ee024a746bb9325
Avery Dyer <avedyer@gmail.com>
2021-09-20 23:06:31 -0400

    wrote adjustColor and added reset functionality

--- script.js
+++ script.js
@@ -1,39 +1,79 @@
 let canvas = document.querySelector(".grid");
 let range = document.querySelector(".range");
 let drawModeButtons = document.querySelectorAll("button");
-let drawMode = "black";
-let canvasSize = 16;
+let drawMode = "Black";
+let pixelCount = 16;
 let pixels = [];
+let rangeCaption = document.querySelector("#rangeCaption");
+let pixelSize;
+let canvasSize;
+let rainbowList = ["FF0000", "#FFFF00", "#00FF00", "#00FFFF", "#FF00FF"]
 
 for (let drawModeButton of drawModeButtons) {
     drawModeButton.addEventListener("click", function() {
         drawMode = drawModeButton.value;
-        console.log(`Draw Mode set to ${drawMode}`);
+        if (drawMode === "Reset"){
+            let pixels = document.querySelectorAll(".pixel");
+            for (let pixel of pixels) {
+                pixel.style.backgroundColor = "#FFFFFF"
+            }
+            console.log("reset")
+        }
+        return false;
     })
 }
 
-let reformatCanvas = function() {
-    canvasSize = range.value;
-    canvas.innerHTML = "";
-    for (let i=0; i<canvasSize**2; i++){
-        canvas.innerHTML += `<div class="pixel"></div>`;
-    }
+let resize = function() {
+    pixelCount = range.value;
+    canvasSize = canvas.offsetWidth;
+    let pixelSize = (canvasSize / pixelCount);
     pixels = document.querySelectorAll(".pixel")
     for (let pixel of pixels) {
-        pixel.addEventListener('onhover', adjustColor());
+        pixel.style.width = pixelSize + "px";
+        pixel.style.height = pixelSize + "px";
     }
-    console.log(`Canvas reformatted to ${canvasSize} x ${canvasSize}`);
-};
-
-let adjustColor = function() {
-    //TODO
-
+    console.log("Canvas size is " + canvasSize);
+    console.log("Pixel size is " + pixelSize)
+    document.querySelector("#rangeCaption").innerHTML = `${pixelCount} x ${pixelCount}`
 }
 
+let reformatCanvas = function() {
+    canvas.innerHTML = "";
+    for (let i=0; i<pixelCount**2; i++){
+        canvas.innerHTML += `<div class="pixel"></div>`;
+    }
+
+    resize();
+
+    pixels = document.querySelectorAll(".pixel")
+    for (let pixel of pixels) {
+        pixel.addEventListener("mouseover", function(){
+            let color;
+
+            if (drawMode === "Black") {
+            color = "#000000"
+            }
+            else if (drawMode === "Rainbow") {
+                color = rainbowList[Math.floor(Math.random() * 5)];
+            }
+
+            else {
+                color = "#FFFFFF"
+            }
+
+            pixel.style.backgroundColor = color;
+
+            console.log("Pixel color set to " + color);
+        });
+    }
+};
+
+
 range.addEventListener("change", function() { 
-    canvasSize = range.value;
-    console.log(`Range slider moved to ${canvasSize} x ${canvasSize}`)
+    pixelCount = range.value;
     reformatCanvas();
 })
 
 window.addEventListener("DOMContentLoaded", reformatCanvas());
+
+canvas.onresize = console.log("canvas resized");
