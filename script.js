2cc734aaaf0e03e456eff1e8f72affd4e72716d5
Avery Dyer <avedyer@gmail.com>
2021-09-22 19:30:41 -0400

    retooled the resizing functionality and fixed random rainbow colors. may tweak sizing further to prevent canvas resizing based on pixel count

--- script.js
+++ script.js
@@ -2,22 +2,27 @@ let canvas = document.querySelector(".grid");
 let range = document.querySelector(".range");
 let drawModeButtons = document.querySelectorAll("button");
 let drawMode = "Black";
-let pixelCount = 16;
 let pixels = [];
 let rangeCaption = document.querySelector("#rangeCaption");
 let pixelSize;
 let canvasSize;
-let rainbowList = ["FF0000", "#FFFF00", "#00FF00", "#00FFFF", "#FF00FF"]
+let rainbowList = ["#FF0000", "#FFFF00", "#00FF00", "#00FFFF", "#FF00FF"]
+
 
 for (let drawModeButton of drawModeButtons) {
     drawModeButton.addEventListener("click", function() {
+
+        //Sets drawMode value for later color selection, returns false to prevent refreshing.
+
         drawMode = drawModeButton.value;
         if (drawMode === "Reset"){
-            let pixels = document.querySelectorAll(".pixel");
+
+            //Reset button does not change drawMode, but resets every pixel to white.
+
+            pixels = document.querySelectorAll(".pixel");
             for (let pixel of pixels) {
-                pixel.style.backgroundColor = "#FFFFFF"
+                pixel.style.backgroundColor = "#FFFFFF";
             }
-            console.log("reset")
         }
         return false;
     })
@@ -26,27 +31,36 @@ for (let drawModeButton of drawModeButtons) {
 let resize = function() {
     pixelCount = range.value;
     canvasSize = canvas.offsetWidth;
-    let pixelSize = (canvasSize / pixelCount);
+
+    //pixelSize is changed as a function of canvasSize, shrunk slightly to avoid overflow.
+    //only width value is used for calculations as canvas is a constant square.
+
+    let pixelSize = (canvasSize / pixelCount) - 1;
     pixels = document.querySelectorAll(".pixel")
     for (let pixel of pixels) {
         pixel.style.width = pixelSize + "px";
-        pixel.style.height = pixelSize + "px";
+        pixel.style.height = pixelSize   + "px";
     }
-    console.log("Canvas size is " + canvasSize);
-    console.log("Pixel size is " + pixelSize)
-    document.querySelector("#rangeCaption").innerHTML = `${pixelCount} x ${pixelCount}`
+
+    document.querySelector("#rangeCaption").innerHTML = `${pixelCount} x ${pixelCount}`;    
 }
 
 let reformatCanvas = function() {
+    pixelCount = range.value;
+    //writes new pixels when canvas is reset.
+
     canvas.innerHTML = "";
     for (let i=0; i<pixelCount**2; i++){
-        canvas.innerHTML += `<div class="pixel"></div>`;
+        canvas.innerHTML += `<div class="pixel" style="background-color: white;"></div>`;
     }
 
+    //resize canvas due to pixel count changing.
     resize();
 
     pixels = document.querySelectorAll(".pixel")
     for (let pixel of pixels) {
+        //event listeners to change pixel color on mouseover
+
         pixel.addEventListener("mouseover", function(){
             let color;
 
@@ -54,26 +68,29 @@ let reformatCanvas = function() {
             color = "#000000"
             }
             else if (drawMode === "Rainbow") {
-                color = rainbowList[Math.floor(Math.random() * 5)];
+                //selects rancomly from an array of colors to create rainbow
+
+                color = rainbowList[Math.floor(Math.random() * 6)];
             }
 
             else {
+                //last possible drawMode is eraser, set to white.
+
                 color = "#FFFFFF"
             }
 
             pixel.style.backgroundColor = color;
-
-            console.log("Pixel color set to " + color);
         });
     }
 };
 
 
 range.addEventListener("change", function() { 
-    pixelCount = range.value;
     reformatCanvas();
 })
 
 window.addEventListener("DOMContentLoaded", reformatCanvas());
 
-canvas.onresize = console.log("canvas resized");
+window.onresize = function() {
+    resize();
+};
